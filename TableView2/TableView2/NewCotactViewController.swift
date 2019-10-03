02d46f9ce3be9ebe4f567655f99049fb26e259a0
Henry Chung <hchung11@uncc.edu>
2019-10-03 11:37:48 -0400

    idk differs remote

---
+++ TableView2/TableView2/NewCotactViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewCotactViewController.swift
+//  TableView2
+//
+//  Created by Jerry Zacharias on 10/3/19.
+//  Copyright © 2019 Hyun Shik Chung. All rights reserved.
+//
+
+import UIKit
+
+class NewCotactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
