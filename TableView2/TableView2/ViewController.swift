58e6a9dc7463631d5e790b7d5acb1e47a985e9c5
Henry Chung <hchung11@uncc.edu>
2019-10-03 10:20:59 -0400

    commit and push first time from Xcode

---
+++ TableView2/TableView2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView2
+//
+//  Created by Jerry Zacharias on 10/3/19.
+//  Copyright © 2019 Hyun Shik Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
